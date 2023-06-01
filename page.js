6142c0f902051c618cb74d0e838b161867ef84da
Rana Auliana <aulianarana@gmail.com>
2023-06-01 19:04:43 +0800

    part 3

--- page.js
+++ page.js
@@ -1,22 +1,45 @@
 function pagination(totalPage, currentPage) {
   let arr = [];
 
-  if (currentPage < 4) {
-    for (let i = 1; i < currentPage + 2; i++) {
-      arr.push(i);
+  if (currentPage > 0 && currentPage <= totalPage) {
+    // page 1 - 3
+    if (currentPage < 4) {
+      for (let i = 1; i < currentPage + 2; i++) {
+        arr.push(i);
+      }
+      console.log(`${arr.join(" ")} ... ${totalPage}`);
+
+      // page 4 - seterusnya
+    } else if (currentPage >= 4 && currentPage < totalPage - 2) {
+      for (let i = currentPage - 1; i < currentPage + 2; i++) {
+        arr.push(i);
+      }
+      console.log(`1 ... ${arr.join(" ")} ... ${totalPage}`);
+
+      // page totalpage - 1
+    } else if (currentPage == totalPage - 1 && currentPage < totalPage) {
+      for (let i = currentPage - 1; i < currentPage + 2; i++) {
+        arr.push(i);
+      }
+      console.log(`1 ... ${arr.join(" ")}`);
+
+      // page totalpage - 2
+    } else if (currentPage == totalPage - 2 && currentPage < totalPage) {
+      for (let i = currentPage - 1; i < currentPage + 3; i++) {
+        arr.push(i);
+      }
+      console.log(`1 ... ${arr.join(" ")}`);
+
+      // page terakhir
+    } else if (currentPage == totalPage) {
+      for (let i = currentPage - 2; i < currentPage + 1; i++) {
+        arr.push(i);
+      }
+      console.log(`1 ... ${arr.join(" ")}`);
     }
-    console.log(`${arr.join(" ")} ... ${totalPage}`);
-  } else if (currentPage > 4 && currentPage != totalPage) {
-    for (let i = currentPage - 2; i < currentPage + 2; i++) {
-      arr.push(i);
-    }
-    console.log(`1 ... ${arr.join(" ")} ... ${totalPage}`);
-  } else if (currentPage <= totalPage && currentPage <= totalPage - 1) {
-    for (let i = currentPage - 2; i < currentPage + 1; i++) {
-      arr.push(i);
-    }
-    console.log(`1 ... ${arr.join(" ")}`);
+  } else {
+    console.log("page doesn't exist");
   }
 }
 
-pagination(29, 28);
+pagination(29, -1);
--- tes.js
+++ tes.js
@@ -1,7 +1,7 @@
 function string(limit) {
   let kalimat = "berburu ubur-ubur syalala lala";
 
-  let slicing = kalimat.slice(0, limit - 1);
+  let slicing = kalimat.slice(0, limit);
 
   let spliting = slicing.split(" ");
 
@@ -9,15 +9,17 @@ function string(limit) {
 
   let final = [];
 
-  if (kalimat[slicing.length] != " ") {
+  if (spliting[0].split("").length != kata[0].split("").length) {
+    return "not found";
+  } else if (kalimat[slicing.length] != " ") {
     for (let i = 0; i < spliting.length - 1; i++) {
       final.push(kata[i]);
     }
   } else {
-    return kalimat.slice(0, limit - 1);
+    return kalimat.slice(0, limit);
   }
   return final.join(" ");
 }
 
-let jawaban = string(16);
+let jawaban = string(7);
 console.log(jawaban);
